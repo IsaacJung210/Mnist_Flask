4c8cd9436cabf79e9d45f03d859836a9a8149b82
IsaacJung210 <dltkr0210@naver.com>
2022-04-15 12:07:40 +0900

    mnist 폼 완성

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+from importlib.resources import path
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/mnist',methods=['GET', 'POST'])
+
+@app.route('/mnist', methods=['GET', 'POST'])
 def mnist():
     if request.method == 'GET':
         return render_template('mnistform.html')
     else:
-        pass
-
+        f = request.files['mnistfile']
+        path = os.path.dirname(__file__)+'/upload/'+f.filename
+        f.save(path)
+        return "성공!!"
 
 
 if __name__ == '__main__':
